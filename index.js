ffbc31028f99aa480aef9662deb7348dacbf67de
Adoni1234 <adonibrito999@gmail.com>
2025-12-07 12:28:50 -0400

    change

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
+const app = require("./app");
 
-app.get("/", (req, res) => {
-  res.send("Hola Mundo desde DevOps!");
-});
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en puerto ${port}`);
 });
-
-module.exports = app;
